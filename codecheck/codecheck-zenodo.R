3cae09f576e6b188dfcd2f72f60c8dda3917a3ff
Stephen J. Eglen <sje30@cam.ac.uk>
2024-03-15 22:28:50 +0000

    working on certificate (API broken still it seems)

--- codecheck/codecheck-zenodo.R
+++ codecheck/codecheck-zenodo.R
@@ -1,7 +1,8 @@
 ## Zenodo deposit; see vignette("codecheck_overview.Rmd")
 
 library("codecheck")
-
+library(yaml)
+library(zen4R)
 ## This assumes your working directory is the codecheck directory
 metadata = read_yaml( "../codecheck.yml")
 
@@ -22,6 +23,7 @@ zenodo <- ZenodoManager$new(token = my_token)
 
 record = get_zenodo_record(metadata$report)
 codecheck:::set_zenodo_metadata(zenodo, record, metadata)
+set_zenodo_metadata1(zenodo, record, metadata)
 
 ## If you have already uploaded the certificate once, you will need to
 ## delete it via the web page before uploading it again.
@@ -32,3 +34,39 @@ codecheck:::set_zenodo_metadata(zenodo, record, metadata)
 
 ## Now go to zenodo and check the record (the URL is printed
 ## by set_zenodo_metadata() ) and then publish.
+
+set_zenodo_metadata1 <- 
+function (zen, record, metadata) 
+{
+    draft <- zen$getDepositionById(record)
+    if (is.null(draft)) {
+        draft <- zen$getRecordById(record)
+    }
+    if (is.null(draft)) 
+        stop("Neither deposition nor record found for ID ", record)
+    draft$setPublicationType("report")
+    draft$setCommunities(communities = c("codecheck"))
+    draft$setTitle(paste("CODECHECK certificate", metadata$certificate))
+    draft$setLanguage(language = "eng")
+    draft$metadata$creators = NULL
+    num_creators = length(metadata$codechecker)
+    for (i in 1:num_creators) {
+        draft$addCreator(name = metadata$codechecker[[i]]$name, 
+            orcid = metadata$codechecker[[i]]$ORCID)
+    }
+    description_text = paste("CODECHECK certificate for paper:", 
+        metadata$paper$title)
+    repo_url = gsub("[<>]", "", metadata$repository)
+    description_text = paste(description_text, sprintf("<p><p>Repository: <a href=\"%s\">%s</a>", 
+        repo_url, repo_url))
+    draft$setDescription(description_text)
+    draft$setKeywords(keywords = c("CODECHECK"))
+    draft$setNotes(notes = c("See file LICENSE for license of the contained code. The report document codecheck.pdf is published under CC-BY 4.0 International."))
+    draft$setAccessRight(accessRight = "open")
+    ##draft$setLicense(licenseId = "other-open")
+    draft$addRelatedIdentifier(relation = "isSupplementTo", identifier = metadata$repository)
+    draft$addRelatedIdentifier(relation = "isSupplementTo", identifier = metadata$paper$reference)
+    draft <- zen$depositRecord(draft)
+    cat(paste0("Check your record online at https://zenodo.org/deposit/", 
+        record, "\n"))
+}
